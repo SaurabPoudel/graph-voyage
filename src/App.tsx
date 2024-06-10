c46108558e2f33b43882378a337d6969f1598f2b
SaurabPoudel <poudelsaurab20@gmail.com>
2024-06-10 19:14:33 +0545

    halka fun

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Model as Ship } from "./components/assetsComponents/ShipLight";
 import Trees from "./components/Trees";
 import { OrbitControls } from "@react-three/drei";
 import { useMemo, useState, useEffect } from "react";
+import SoundOff from "./components/SoundOff";
+import SoundOn from "./components/SoundOn";
+// import PlayGameMenu from "./components/PlayGameMenu";
 
 function App() {
   const [xpos, setXpos] = useState(0);
@@ -29,15 +32,14 @@ function App() {
     setPlaying(!playing);
   };
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.key === "ArrowRight") {
-      setXpos(xpos + 1);
-    } else if (event.key === "ArrowLeft") {
-      setXpos(xpos - 1);
-    }
-  };
-
   useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowRight") {
+        setXpos(xpos + 0.02);
+      } else if (event.key === "ArrowLeft") {
+        setXpos(xpos - 0.02);
+      }
+    };
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
@@ -45,21 +47,24 @@ function App() {
   }, [xpos]);
 
   return (
-    <div className="h-screen bg-black flex justify-center items-center relative">
-      <button
-        className="absolute top-4 right-4 bg-green-500 py-2 px-3 z-10"
-        onClick={handleButtonClick}
-      >
-        {playing ? "Pause" : "Play"} Sound
-      </button>
-      <Canvas camera={{ position: [10, 5, 2] }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <Ship position={[xpos, 0, 0]} />
-        <Trees count={20} spacing={10} speed={0.1} />
-        <OrbitControls />
-      </Canvas>
-    </div>
+    <>
+      <div className="h-screen bg-black flex justify-center items-center ">
+        <div
+          className="absolute w-5 h-5 top-4 right-4 z-10"
+          onClick={handleButtonClick}
+        >
+          {playing ? <SoundOn /> : <SoundOff />}
+        </div>
+        <Canvas camera={{ position: [10, 5, 2] }}>
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <Ship position={[xpos, 0, 0]} />
+          <Trees count={20} spacing={10} speed={0.02} />
+          <OrbitControls />
+        </Canvas>
+      </div>
+      {/* <PlayGameMenu /> */}
+    </>
   );
 }
 
---
+++ src/components/PlayGameMenu.tsx
@@ -0,0 +1,7 @@
+export default function PlayGameMenu() {
+  return (
+    <div className="absolute w-1/2 h-2/3 top-1/2 left-1/3 border-green-500 flex justify-center items-center">
+      <button className="bg-green-500">Play Game</button>
+    </div>
+  );
+}
